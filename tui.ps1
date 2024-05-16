3b6f08d88bd24884a27462fce15e73654754095e
meyyh <cooperrs123@gmail.com>
2024-05-16 15:32:21 -0700

    update

--- tui.ps1
+++ tui.ps1
@@ -19,7 +19,8 @@ function Show-SimpleMenu ([array]$MenuOptions, [string]$Title ='Choose an option
         $keycode = [Console]::ReadKey($true)
         If ($keyCode.Key -eq [ConsoleKey]::UpArrow -and $highlighted -gt 0 ) {$highlighted--}
         If ($keycode.Key -eq [ConsoleKey]::DownArrow -and $highlighted -lt $MenuOptions.Length - 1) {$highlighted++}
-    }While($keyCode.Key -ne [ConsoleKey]::Enter -and $keycode.Key -ne [ConsoleKey]::Escape )
+    }While($keyCode.Key -ne [ConsoleKey]::Enter -and $keycode.Key -ne [ConsoleKey]::Escape)
+    
     If($keyCode.Key -eq [ConsoleKey]::Enter){ $MenuOptions[$highlighted] }
 }
 
@@ -37,7 +38,7 @@ function set-address(){
     $interfaces = Get-NetIPInterface -AddressFamily IPv4 | Select-Object -ExpandProperty InterfaceAlias
     $interface = Show-SimpleMenu @($interfaces)
 
-    $adapter = Get-NetAdapter | ? {$_.Name -eq $interface}
+    $adapter = Get-NetAdapter | Where-Object {$_.Name -eq $interface}
     $currIP = $($adapter | Get-NetIPAddress -AddressFamily IPv4).IPAddress
     $currMask = $($adapter | Get-NetIPAddress -AddressFamily IPv4).PrefixLength
     write-host "current ip is $currIP\$currMask"
@@ -70,17 +71,30 @@ function set-address(){
         }
     }
 
+    $oldDns = Get-DnsClientServerAddress -InterfaceIndex $adapter.InterfaceIndex -AddressFamily ipv4
+    write-host "to use old dns ($oldDns) press enter"
+    $newDns = Read-Host -Prompt "Enter new dns ip"
+    if ([string]::IsNullOrWhiteSpace($newDns)) {
+        $newDns = $oldDns
+    } else {
+        if ($newDns -notmatch $ipv4Regex) {
+            Write-Host "Invalid IPv4 address" -ForegroundColor Red
+            set-address
+        }
+    }
+    Set-DnsClientServerAddress -InterfaceIndex $adapter.InterfaceIndex -ServerAddresses $newDns -Validate 
+
     #confirm before applying ip
     $yon = $(Write-Host "new ip is $newIP\$newMask confirm (y / n)" -ForegroundColor Yellow; Read-Host)
     $yon = $yon.ToLower()
     if ($yon -eq "yes" -or $yon -eq "y"){
         If (($adapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
             $adapter | Remove-NetIPAddress -AddressFamily IPv4 -Confirm:$false
-           }
-           If (($adapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
+        }
+        If (($adapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
             $adapter | Remove-NetRoute -AddressFamily ipv4 -Confirm:$false
-           }
-        Set-NetIPAddress -InterfaceIndex $adapter.InterfaceIndex -IPAddress $newIP -PrefixLength $newMask -AddressFamily ipv4
+        }
+        New-NetIPAddress -InterfaceIndex $adapter.InterfaceIndex -IPAddress $newIP -PrefixLength $newMask -DefaultGateway $newGate -AddressFamily ipv4
     } else {
         set-address
     }
