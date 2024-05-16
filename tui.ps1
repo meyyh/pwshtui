6e169a5fbd72413e069a537118f98badcd4ab2b4
Cooper Sanders <94275329+meyyh@users.noreply.github.com>
2024-05-16 02:14:10 -0500

    Update tui.ps1

--- tui.ps1
+++ tui.ps1
@@ -20,12 +20,76 @@ function Show-SimpleMenu ([array]$MenuOptions, [string]$Title ='Choose an option
         If ($keyCode.Key -eq [ConsoleKey]::UpArrow -and $highlighted -gt 0 ) {$highlighted--}
         If ($keycode.Key -eq [ConsoleKey]::DownArrow -and $highlighted -lt $MenuOptions.Length - 1) {$highlighted++}
     }While($keyCode.Key -ne [ConsoleKey]::Enter -and $keycode.Key -ne [ConsoleKey]::Escape )
-    If($keyCode.Key -eq [ConsoleKey]::Enter){ &$MenuOptions[$highlighted] }
+    If($keyCode.Key -eq [ConsoleKey]::Enter){ $MenuOptions[$highlighted] }
 }
 
 function network(){
-    Write-Host "hi mum"
+    $ret = Show-SimpleMenu @('📁ip','temp')
+    Invoke-Expression $ret
 }
 
-Show-SimpleMenu @('network','domain','reg')
+function 📁ip(){
+    $ret = Show-SimpleMenu @('set-address','set-dns') -Title sugma
+    Invoke-Expression $ret
+}
 
+function set-address(){
+    $interfaces = Get-NetIPInterface -AddressFamily IPv4 | Select-Object -ExpandProperty InterfaceAlias
+    $interface = Show-SimpleMenu @($interfaces)
+
+    $adapter = Get-NetAdapter | ? {$_.Name -eq $interface}
+    $currIP = $($adapter | Get-NetIPAddress -AddressFamily IPv4).IPAddress
+    $currMask = $($adapter | Get-NetIPAddress -AddressFamily IPv4).PrefixLength
+    write-host "current ip is $currIP\$currMask"
+
+    
+    $newMask = Read-Host -Prompt "Enter new subnet mask 24/16/8"
+    $newMask = [int]$newMask  # Cast to int because powershell is dumb
+    if ($newMask -gt 32 -or $newMask -lt 0) {
+        Write-Host "Ivalid network mask (0-32)" -ForegroundColor Red
+        set-address
+    }
+
+    $ipv4Regex = "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+    $newIP = Read-Host -Prompt "Enter new ipv4 address"
+    if ($newIP -notmatch $ipv4Regex) {
+        Write-Host "Invalid IPv4 address" -ForegroundColor Red
+        set-address
+    }
+
+    $oldGate =  $($adapter | Get-NetIPConfiguration).IPv4DefaultGateway.NextHop
+    write-host "to use old gateway ($oldGate) press enter"
+    $newGate = Read-Host -Prompt "Enter new ipv4 gateway"
+    write-host " here is new gate $newGate"
+    if ([string]::IsNullOrWhiteSpace($newGate)) {
+        $newGate = $oldGate
+    } else {
+        if ($newGate -notmatch $ipv4Regex) {
+            Write-Host "Invalid IPv4 address" -ForegroundColor Red
+            set-address
+        }
+    }
+
+    #confirm before applying ip
+    $yon = $(Write-Host "new ip is $newIP\$newMask confirm (y / n)" -ForegroundColor Yellow; Read-Host)
+    $yon = $yon.ToLower()
+    if ($yon -eq "yes" -or $yon -eq "y"){
+        If (($adapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
+            $adapter | Remove-NetIPAddress -AddressFamily IPv4 -Confirm:$false
+           }
+           If (($adapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
+            $adapter | Remove-NetRoute -AddressFamily ipv4 -Confirm:$false
+           }
+        Set-NetIPAddress -InterfaceIndex $adapter.InterfaceIndex -IPAddress $newIP -PrefixLength $newMask -AddressFamily ipv4
+    } else {
+        set-address
+    }
+}
+
+function search(){
+
+}
+
+
+$inital = Show-SimpleMenu @('network','domain','reg','search')
+Invoke-Expression $inital
