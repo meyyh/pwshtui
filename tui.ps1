9c68bdfc62fbeb14ee0674fb8f273fba8578d92c
Cooper Sanders <94275329+meyyh@users.noreply.github.com>
2024-05-16 18:22:46 -0500

    made it work with powershell 5.1

--- tui.ps1
+++ tui.ps1
@@ -1,109 +1,122 @@
-function Show-SimpleMenu ([array]$MenuOptions, [string]$Title ='Choose an option'){
-    $maxLength = ($MenuOptions | Measure-Object -Maximum -Property Length).Maximum #get longest string length
-    If($maxLength -lt $Title.Length){$maxLength = $Title.Length}
-    $highlighted = 0 
+# Ensure the console uses UTF-8 encoding
+[Console]::OutputEncoding = [System.Text.Encoding]::UTF8
+$topLeft = [char]::ConvertFromUtf32(0x250C)
+$topRight = [char]::ConvertFromUtf32(0x2510)
+$botLeft = [char]::ConvertFromUtf32(0x2514)
+$botRight = [char]::ConvertFromUtf32(0x2518)
+$horiz = [char]::ConvertFromUtf32(0x2500)
+$vert = [char]::ConvertFromUtf32(0x2502)
+function Show-SimpleMenu {
+    param (
+        [array]$MenuOptions,
+        [string]$Title = 'Choose an option'
+    )
+    $maxLength = ($MenuOptions | Measure-Object -Maximum -Property Length).Maximum # Get longest string length
+    if ($maxLength -lt $Title.Length) { $maxLength = $Title.Length }
+    $highlighted = 0
     $MenuTop = [Console]::CursorTop
-    Do{
+    Do {
         [Console]::CursorTop = $MenuTop
-        Write-Host "┌$($Title.PadRight($maxLength,'─'))┐" 
-        for ($i = 0; $i -lt $MenuOptions.Length;$i++) {
-            Write-Host "│" -NoNewLine
+        Write-Host "$topLeft$($Title.PadRight($maxLength,$horiz))$topRight"
+        for ($i = 0; $i -lt $MenuOptions.Length; $i++) {
+            Write-Host "$vert" -NoNewLine
             if ($i -eq $highlighted) {
-                Write-Host "$(([string]$MenuOptions[$i]).PadRight($maxLength,' '))" -fore $host.UI.RawUI.BackgroundColor -back $host.UI.RawUI.ForegroundColor -NoNewline
+                Write-Host "$(([string]$MenuOptions[$i]).PadRight($maxLength,' '))" -ForegroundColor Black -BackgroundColor White -NoNewLine
             } else {
-                Write-Host "$(([string]$MenuOptions[$i]).PadRight($maxLength,' '))" -fore $host.UI.RawUI.ForegroundColor -back $host.UI.RawUI.BackgroundColor -NoNewline
+                Write-Host "$(([string]$MenuOptions[$i]).PadRight($maxLength,' '))" -NoNewLine
             }
-            Write-Host "│"
+            Write-Host "$vert"
         }
-        Write-Host "└$('─' * ($maxLength))┘"
+        Write-Host "$botLeft$($horiz * ($maxLength))$botRight"
         $keycode = [Console]::ReadKey($true)
-        If ($keyCode.Key -eq [ConsoleKey]::UpArrow -and $highlighted -gt 0 ) {$highlighted--}
-        If ($keycode.Key -eq [ConsoleKey]::DownArrow -and $highlighted -lt $MenuOptions.Length - 1) {$highlighted++}
-    }While($keyCode.Key -ne [ConsoleKey]::Enter -and $keycode.Key -ne [ConsoleKey]::Escape)
-    
-    If($keyCode.Key -eq [ConsoleKey]::Enter){ $MenuOptions[$highlighted] }
+        if ($keyCode.Key -eq [ConsoleKey]::UpArrow -and $highlighted -gt 0) { $highlighted-- }
+        if ($keycode.Key -eq [ConsoleKey]::DownArrow -and $highlighted -lt $MenuOptions.Length - 1) { $highlighted++ }
+    } while ($keyCode.Key -ne [ConsoleKey]::Enter -and $keycode.Key -ne [ConsoleKey]::Escape)
+
+    if ($keyCode.Key -eq [ConsoleKey]::Enter) { return $MenuOptions[$highlighted] }
 }
 
-function network(){
-    $ret = Show-SimpleMenu @('📁ip','temp')
+function network {
+    $ret = Show-SimpleMenu @('ip', 'temp')
     Invoke-Expression $ret
 }
 
-function 📁ip(){
-    $ret = Show-SimpleMenu @('set-address','set-dns') -Title sugma
+function ip {
+    $ret = Show-SimpleMenu @('set-address', 'set-dns') -Title 'sugma'
     Invoke-Expression $ret
 }
 
-function set-address(){
+function set-address {
     $interfaces = Get-NetIPInterface -AddressFamily IPv4 | Select-Object -ExpandProperty InterfaceAlias
     $interface = Show-SimpleMenu @($interfaces)
 
-    $adapter = Get-NetAdapter | Where-Object {$_.Name -eq $interface}
-    $currIP = $($adapter | Get-NetIPAddress -AddressFamily IPv4).IPAddress
-    $currMask = $($adapter | Get-NetIPAddress -AddressFamily IPv4).PrefixLength
-    write-host "current ip is $currIP\$currMask"
+    $adapter = Get-NetAdapter | Where-Object { $_.Name -eq $interface }
+    $currIP = ($adapter | Get-NetIPAddress -AddressFamily IPv4).IPAddress
+    $currMask = ($adapter | Get-NetIPAddress -AddressFamily IPv4).PrefixLength
+    Write-Host "Current IP is $currIP/$currMask"
 
-    
-    $newMask = Read-Host -Prompt "Enter new subnet mask 24/16/8"
-    $newMask = [int]$newMask  # Cast to int because powershell is dumb
+    $newMask = Read-Host -Prompt "Enter new subnet mask (24/16/8)"
+    $newMask = [int]$newMask  # Cast to int because PowerShell is strict with types
     if ($newMask -gt 32 -or $newMask -lt 0) {
-        Write-Host "Ivalid network mask (0-32)" -ForegroundColor Red
+        Write-Host "Invalid network mask (0-32)" -ForegroundColor Red
         set-address
+        return
     }
 
     $ipv4Regex = "^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
-    $newIP = Read-Host -Prompt "Enter new ipv4 address"
+    $newIP = Read-Host -Prompt "Enter new IPv4 address"
     if ($newIP -notmatch $ipv4Regex) {
         Write-Host "Invalid IPv4 address" -ForegroundColor Red
         set-address
+        return
     }
 
-    $oldGate =  $($adapter | Get-NetIPConfiguration).IPv4DefaultGateway.NextHop
-    write-host "to use old gateway ($oldGate) press enter"
-    $newGate = Read-Host -Prompt "Enter new ipv4 gateway"
-    write-host " here is new gate $newGate"
+    $oldGate = ($adapter | Get-NetIPConfiguration).IPv4DefaultGateway.NextHop
+    Write-Host "To use old gateway ($oldGate) press enter"
+    $newGate = Read-Host -Prompt "Enter new IPv4 gateway"
     if ([string]::IsNullOrWhiteSpace($newGate)) {
         $newGate = $oldGate
     } else {
         if ($newGate -notmatch $ipv4Regex) {
             Write-Host "Invalid IPv4 address" -ForegroundColor Red
             set-address
+            return
         }
     }
 
-    $oldDns = Get-DnsClientServerAddress -InterfaceIndex $adapter.InterfaceIndex -AddressFamily ipv4
-    write-host "to use old dns ($oldDns) press enter"
-    $newDns = Read-Host -Prompt "Enter new dns ip"
+    $oldDns = Get-DnsClientServerAddress -InterfaceIndex $adapter.InterfaceIndex -AddressFamily IPv4
+    Write-Host "To use old DNS ($oldDns) press enter"
+    $newDns = Read-Host -Prompt "Enter new DNS IP"
     if ([string]::IsNullOrWhiteSpace($newDns)) {
         $newDns = $oldDns
     } else {
         if ($newDns -notmatch $ipv4Regex) {
             Write-Host "Invalid IPv4 address" -ForegroundColor Red
             set-address
+            return
         }
     }
     Set-DnsClientServerAddress -InterfaceIndex $adapter.InterfaceIndex -ServerAddresses $newDns -Validate 
 
-    #confirm before applying ip
-    $yon = $(Write-Host "new ip is $newIP\$newMask confirm (y / n)" -ForegroundColor Yellow; Read-Host)
+    # Confirm before applying IP
+    $yon = Read-Host "New IP is $newIP/$newMask confirm (y / n)"
     $yon = $yon.ToLower()
-    if ($yon -eq "yes" -or $yon -eq "y"){
-        If (($adapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
+    if ($yon -eq "yes" -or $yon -eq "y") {
+        if (($adapter | Get-NetIPConfiguration).IPv4Address.IPAddress) {
             $adapter | Remove-NetIPAddress -AddressFamily IPv4 -Confirm:$false
         }
-        If (($adapter | Get-NetIPConfiguration).Ipv4DefaultGateway) {
-            $adapter | Remove-NetRoute -AddressFamily ipv4 -Confirm:$false
+        if (($adapter | Get-NetIPConfiguration).IPv4DefaultGateway) {
+            $adapter | Remove-NetRoute -AddressFamily IPv4 -Confirm:$false
         }
-        New-NetIPAddress -InterfaceIndex $adapter.InterfaceIndex -IPAddress $newIP -PrefixLength $newMask -DefaultGateway $newGate -AddressFamily ipv4
+        New-NetIPAddress -InterfaceIndex $adapter.InterfaceIndex -IPAddress $newIP -PrefixLength $newMask -DefaultGateway $newGate -AddressFamily IPv4
     } else {
         set-address
     }
 }
 
-function search(){
-
+function search {
+    # Add the functionality for the search here
 }
 
-
-$inital = Show-SimpleMenu @('network','domain','reg','search')
-Invoke-Expression $inital
+$initial = Show-SimpleMenu @('network', 'domain', 'reg', 'search')
+Invoke-Expression $initial
